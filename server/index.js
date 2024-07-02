56c68cd4280b82580c45d25b1efc7719d02c2f12
Bilal khan <142602476+bilal2077@users.noreply.github.com>
2024-07-02 23:18:07 +0500

    p for push

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
-   const http = require("http");
+const  http = require("http");
+const  fs = require("fs");
+const { url } = require("inspector");
+ 
 
-   const myServer = http.createServer(
-    (req, res) => {
-        console.log("New Req Res");
-        res.end("hello from server");
+const myServer =http.createServer((req,res)=>{
+    const log = `${Date.now()}: ${req.url}  New Req Received \n`
+    fs.appendFile("log.txt", log,(err,data)=>{
+        switch (req.url) {
+            case "/":res.end("Home Page");
+                
+                break
+            case "/about":res.end("i am hello");
+                
+                break
+        
+            default:res.end("404 Not Found");
+               
+        }
+        
     });
+});
 
-myServer.listen(8000, () => console.log("Server Started!"))
-
-   
+myServer.listen(8000, ()=> console.log("sever Started!"));
