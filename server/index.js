629fbc67fb1cf7e2a394ec63d463f02aa1a6aa7e
Bilal khan <142602476+bilal2077@users.noreply.github.com>
2024-07-08 04:24:41 +0500

    start  Express

--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
 const  http = require("http");
 const  fs = require("fs");
-const { url } = require("inspector");
+const  url  = require("url");
  
 
 const myServer =http.createServer((req,res)=>{
-    const log = `${Date.now()}: ${req.url}  New Req Received \n`
+    if (req.url === "/favicon.ico") return res.end();
+    const log = `${Date.now()}: ${req.url}  New Req Received \n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt", log,(err,data)=>{
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case "/":res.end("Home Page");
                 
                 break
-            case "/about":res.end("i am hello");
+            case "/about":
+            const username = myUrl.query.myname
+            res.end(`Hi, ${username}`); 
+                break;
                 
-                break
+                const search = myUrl.query.search_myUrl;
+                res.end("here are your result for "+ search);
         
             default:res.end("404 Not Found");
                
